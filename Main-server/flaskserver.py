dfa8ad2165cf184271c0603944fdbc7609a8e5c9
lovelesh patel <lovelesh06101990@gmail.com>
2022-06-17 01:29:32 -0400

    added WhatsApp message functionality

--- Main-server/flaskserver.py
+++ Main-server/flaskserver.py
@@ -1,11 +1,15 @@
-# code for parsing json file
-# Using flask to make an api
 # import necessary libraries and functions
-from flask import Flask, request, render_template, session, redirect, url_for
+from flask import Flask, request, render_template, session, redirect, url_for, send_from_directory, send_file
 from flask_qrcode import QRcode
 from flask_pymongo import PyMongo
 import time
 import random
+
+# twilio Integration
+from twilio.rest import Client
+import qrcode
+import socket
+import os
 
 ticket_num_length = 16
 m_id_length = 10
@@ -14,15 +18,27 @@ adult_length = 2
 child_length = 2
 valid_hr = 6
 
+hname = socket.gethostname()
+server_IP = '192.168.0.1'
+UPLOAD_FOLDER = 'QRCodes'
+# imageName = ''
+mobile_no = '2269755074'
+
 # creating a Flask app
-app = Flask(__name__)
+app = Flask(__name__, static_folder=UPLOAD_FOLDER)
 QRcode(app)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.secret_key = 'any random string'
 
 # path to the database
 app.config["MONGO_URI"] = "mongodb://localhost:27017/records_db"
 mongodb_client = PyMongo(app)
 db = mongodb_client.db
+
+# Twilio related Data
+account_sid = 'ACc5267b4eff9a0e7e340339148f622423'
+auth_token = '856c1f34e7a750a13f1cafcabbe59669'
+client = Client(account_sid, auth_token)
 
 
 # provides homepage for the visitor
@@ -74,13 +90,42 @@ def getQR():
             result = str(ticket_num).zfill(ticket_num_length) + str(m_id).zfill(m_id_length) + str(s_epoch).zfill(
                 s_epoch_length) + str(adult).zfill(adult_length) + str(child).zfill(child_length)
             print(result)
+
+            # Twilio Integration
+            img = qrcode.make(result)
+            imageName = time.strftime('%Y_%m_%d_%H_%M_%S', time.localtime())
+            imageName = imageName + '.png'
+            img.save(UPLOAD_FOLDER + '/' + imageName)
+            media_url = ['http://' + hname + ':5000/' + UPLOAD_FOLDER + '/' + imageName]
+            print(media_url)
+            # Send WhatsApp Message with QR Code
+            # message = client.messages.create(
+            #     body='Please use this QR Code at Entry',
+            #     media_url = ['http://' + hname + ':5000/' + path + '/' + imageName],
+            #     from_='whatsapp:+14155238886',
+            #     to='whatsapp:+1' + mobile_no
+            # )
+
+            # print(message.sid)
+
         except:
             errors.append("Please verify details in the form")
 
-        return render_template('TicketGenerator.html', errors=errors, result=result, adult=adult, child=child, billNo=ticket_num, adult_total= str(int(adult)*150), child_total= str(int(child)*75), total=str(int(adult) * 150 + int(child) * 75))
+        return render_template('TicketGenerator.html', errors=errors, result=result, adult=adult, child=child,
+                               billNo=ticket_num, adult_total=str(int(adult) * 150), child_total=str(int(child) * 75),
+                               total=str(int(adult) * 150 + int(child) * 75))
 
     else:
         return 'invalid request'
+
+
+@app.route('/QRCodes/<name>', methods=['GET'])
+def get_image(name):
+    print(name)
+    # current_path = app.config['UPLOAD_FOLDER'] + '/' + name
+    # print(current_path)
+    # print(os.path.isfile(current_path))
+    return send_from_directory(app.config['UPLOAD_FOLDER'], name)
 
 
 @app.route('/get_validity', methods=['GET'])
@@ -96,6 +141,7 @@ def get_validity():
         return 'ticket is valid'
     else:
         return 'ticket is invalid'
+
 
 # driver function
 if __name__ == '__main__':
